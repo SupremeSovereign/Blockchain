f2a774fb5490025569d72130c534e1c4994764c6
Nilam Doctor <nilamdoc@gmail.com>
2014-03-25 13:23:03 +0530

    changes

--- app/controllers/QController.php
+++ app/controllers/QController.php
@@ -7,7 +7,7 @@ class QController extends \lithium\action\Controller {
 		return "index";
 	}
 	public function hr24price(){
-			return ".001";
+			return ".01";
 	}
 	public function hr24transactioncount(){
 		return "222";
@@ -24,12 +24,12 @@ class QController extends \lithium\action\Controller {
 	public function totalbc(){
 		$coingreen = new COINGREEN('http://'.COINGREEN_WALLET_SERVER.':'.COINGREEN_WALLET_PORT,COINGREEN_WALLET_USERNAME,COINGREEN_WALLET_PASSWORD);
 		$getinfo = $coingreen->getinfo();
-		return (string)($getinfo['blocks']*5000*1000000);
+		return (string)($getinfo['blocks']*5000*1000000)."00";
 	}
 	public function marketcap(){
 		$coingreen = new COINGREEN('http://'.COINGREEN_WALLET_SERVER.':'.COINGREEN_WALLET_PORT,COINGREEN_WALLET_USERNAME,COINGREEN_WALLET_PASSWORD);
 		$getinfo = $coingreen->getinfo();
-		return (string)($getinfo['blocks']*5000*1000000*.001);
+		return (string)($getinfo['blocks']*5000*.00000001);
 
 	}
 	public function addressbalance(){
