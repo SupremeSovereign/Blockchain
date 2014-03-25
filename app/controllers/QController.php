8c881e8814b179459476ee54253333a0d0600007
Nilam Doctor <nilamdoc@gmail.com>
2014-03-25 13:05:20 +0530

    hashrate

--- app/controllers/QController.php
+++ app/controllers/QController.php
@@ -1,12 +1,13 @@
 <?php
 namespace app\controllers;
+use app\extensions\action\coingreen;
 
 class QController extends \lithium\action\Controller {
 	public function index(){
 		return "index";
 	}
 	public function hr24price(){
-			return "111";
+			return ".001";
 	}
 	public function hr24transactioncount(){
 		return "222";
@@ -16,7 +17,9 @@ class QController extends \lithium\action\Controller {
 		return "333";
 	}
 	public function hashrate(){
-		return "444";
+		$coingreen = new COINGREEN('http://'.COINGREEN_WALLET_SERVER.':'.COINGREEN_WALLET_PORT,COINGREEN_WALLET_USERNAME,COINGREEN_WALLET_PASSWORD);
+		$getinfo = $coingreen->getinfo();
+		return (string)($getinfo['blocks']*5000*1000000);
 	}
 	public function totalbc(){
 		return "555";
